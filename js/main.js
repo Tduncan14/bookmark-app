c4641bde4cae8b68a816934e1b8cdb3e771b2fcc
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-11-24 13:11:32 -0600

    keep moving forward

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
 // Listen for form submit
+document.getElementById('myForm').addEventListener('submit',saveBookMark);
+
+// saveuninstall_node.s
+function saveBookMark(e){
+    console.log("hey");
+    //get from value
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl=document.getElementById('siteUrl').value;
+    console.log(siteName);
+
+    // object that saves to local storage
+
+    var bookMark ={
+        name: siteName,
+        url: siteUrl
+    }
+
+
+    //local storage only store strings
+
+    localStorage.setItem('test','Treek World');
+    console.log(localStorage.getItem('test'));
+    // to delete from local storage
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
+
+
+  
+    console.log(bookMark);
+
+    // prevent from submitting
+    e.preventDefault();
+}
